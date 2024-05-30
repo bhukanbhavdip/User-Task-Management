1469e0f69bc7a1c323ef6fa326d30c759ce7b356
BHAVDIP BHUKAN <128157200+bhukanbhavdip@users.noreply.github.com>
2024-05-30 16:23:53 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-const Connection = async(username,password)=>{
-    const URL = `mongodb://${username}:${password}@ac-8pgutk0-shard-00-00.wrdidxl.mongodb.net:27017,ac-8pgutk0-shard-00-01.wrdidxl.mongodb.net:27017,ac-8pgutk0-shard-00-02.wrdidxl.mongodb.net:27017/?ssl=true&replicaSet=atlas-4yqj5r-shard-0&authSource=admin&retryWrites=true&w=majority`
+const Connection = async()=>{
+    const URL = `mongodb://bhukanbhavdip3:bhavdip@ac-8pgutk0-shard-00-00.wrdidxl.mongodb.net:27017,ac-8pgutk0-shard-00-01.wrdidxl.mongodb.net:27017,ac-8pgutk0-shard-00-02.wrdidxl.mongodb.net:27017/?ssl=true&replicaSet=atlas-4yqj5r-shard-0&authSource=admin&retryWrites=true&w=majority`
     try {
         await mongoose.connect(URL)
         console.log("database connected..!");
